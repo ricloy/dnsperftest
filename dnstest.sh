c0cc82868a081a0c4d4b74ffcff05f864dbf7c87
ricloy <ricloy@users.noreply.github.com>
2018-05-31 20:46:22 +0200

    Update dnstest.sh
    removed useless use of cat; added some semicolons and absolute paths to command

--- dnstest.sh
+++ dnstest.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
-{ command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
+command -v /usr/bin/bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
+{ command -v /usr/bin/drill >&/dev/null && dig=/usr/bin/drill; } || { command -v /usr/bin/dig >&/dev/null && dig=/usr/bin/dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
 
-
-
-NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
+NAMESERVERS=$(grep '^nameserver' /etc/resolv.conf | cut -d " " -f 2 | sed 's/\(.*\)/&#&/');
 
 PROVIDERS="
 1.1.1.1#cloudflare 
@@ -20,43 +18,42 @@ PROVIDERS="
 176.103.130.132#adguard 
 156.154.70.3#neustar 
 8.26.56.26#comodo
-"
+";
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com";
 
-
-totaldomains=0
-printf "%-18s" ""
+totaldomains=0;
+printf "%-18s" "";
 for d in $DOMAINS2TEST; do
-    totaldomains=$((totaldomains + 1))
-    printf "%-8s" "test$totaldomains"
+    totaldomains=$((totaldomains + 1));
+    printf "%-8s" "test$totaldomains";
 done
-printf "%-8s" "Average"
-echo ""
+printf "%-8s" "Average";
+echo "";
 
 
 for p in $NAMESERVERS $PROVIDERS; do
-    pip=${p%%#*}
-    pname=${p##*#}
-    ftime=0
+    pip=${p%%#*};
+    pname=${p##*#};
+    ftime=0;
 
-    printf "%-18s" "$pname"
+    printf "%-18s" "$pname";
     for d in $DOMAINS2TEST; do
-        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+        ttime=$($dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
         if [ -z "$ttime" ]; then
 	        #let's have time out be 1s = 1000ms
-	        ttime=1000
+	        ttime=1000;
         elif [ "x$ttime" = "x0" ]; then
-	        ttime=1
+	        ttime=1;
 	    fi
 
-        printf "%-8s" "$ttime ms"
-        ftime=$((ftime + ttime))
+        printf "%-8s" "$ttime ms";
+        ftime=$((ftime + ttime));
     done
-    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+    avg=$(bc -lq <<< "scale=2; $ftime/$totaldomains");
 
-    echo "  $avg"
+    echo "  $avg";
 done
 
 
