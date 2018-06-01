186e77d4cc8384f3a5a120c38e0735ea3c78f105
Marco <marcxosm@gmail.com>
2018-06-01 19:29:28 +0200

    added some bashism; randomised domains and DNS checks; added min and max values; included sorting of output by average response time

--- dnstest.sh
+++ dnstest.sh
@@ -1,60 +1,70 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-command -v /usr/bin/bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
-{ command -v /usr/bin/drill >&/dev/null && dig=/usr/bin/drill; } || { command -v /usr/bin/dig >&/dev/null && dig=/usr/bin/dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
+[ -x /usr/bin/bc ] || { echo "bc was not found. Please install bc."; exit 1; }
+{ [ -x /usr/bin/drill ] && dig=/usr/bin/drill; } || { [ -x /usr/bin/dig ] && dig=/usr/bin/dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
 
-NAMESERVERS=$(grep '^nameserver' /etc/resolv.conf | cut -d " " -f 2 | sed 's/\(.*\)/&#&/');
-
-PROVIDERS="
-1.1.1.1#cloudflare 
-4.2.2.1#level3 
-8.8.8.8#google 
-9.9.9.9#quad9 
-80.80.80.80#freenom 
-208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
-8.26.56.26#comodo
-";
+PROVIDERS=("1.1.1.1#cloudflare"
+"4.2.2.1#level3"
+"8.8.8.8#google"
+"9.9.9.9#quad9"
+"80.80.80.80#freenom"
+"208.67.222.123#opendns"
+"199.85.126.20#norton"
+"185.228.168.168#cleanbrowsing"
+"77.88.8.7#yandex"
+"176.103.130.132#adguard"
+"156.154.70.3#neustar"
+"8.26.56.26#comodo");
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com";
+DOMAINS2TEST=(www.google.com
+amazon.com
+facebook.com
+www.instagram.com
+whatsapp.com
+www.youtube.com
+www.reddit.com
+wikipedia.org
+twitter.com
+gmail.com
+www.google.it
+www.repubblica.it
+www.corriere.it
+www.lemonde.fr
+www.elmundo.es
+www.baidu.com);
 
-totaldomains=0;
-printf "%-18s" "";
-for d in $DOMAINS2TEST; do
-    totaldomains=$((totaldomains + 1));
-    printf "%-8s" "test$totaldomains";
-done
-printf "%-8s" "Average";
-echo "";
+randomised_domain_indexes=$(for ((i=0;i<${#DOMAINS2TEST[@]};i++)); do echo $i; done | sort -R)
+randomised_DNS_indexes=$(for ((i=0;i<${#PROVIDERS[@]};i++)); do echo $i; done | sort -R)
 
-
-for p in $NAMESERVERS $PROVIDERS; do
+for i in $randomised_DNS_indexes; do
+    p=${PROVIDERS[i]};
     pip=${p%%#*};
     pname=${p##*#};
     ftime=0;
-
+    
+    min=9999;
+    max=0;
+    
     printf "%-18s" "$pname";
-    for d in $DOMAINS2TEST; do
-        ttime=$($dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
+    for j in $randomised_domain_indexes; do
+        ttime=$($dig +tries=1 +time=2 +stats @$pip ${DOMAINS2TEST[j]} |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
         if [ -z "$ttime" ]; then
-	        #let's have time out be 1s = 1000ms
-	        ttime=1000;
+	        #let's have time out be 1s = 9999ms
+	        ttime=9999;
         elif [ "x$ttime" = "x0" ]; then
 	        ttime=1;
-	    fi
+        fi
 
-        printf "%-8s" "$ttime ms";
         ftime=$((ftime + ttime));
+	if (( ttime > max)); then
+		max=$ttime;
+	fi
+	if ((ttime < min)); then
+		min=$ttime;
+	fi
     done
-    avg=$(bc -lq <<< "scale=2; $ftime/$totaldomains");
-
-    echo "  $avg";
-done
-
-
-exit 0;
+    avg=$(bc -lq <<< "scale=2; $ftime/${#DOMAINS2TEST[@]}");
+  
+    echo "avg: $avg ms (min: $min ms; max: $max ms)";
+done | sort -n -k3
